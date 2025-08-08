e7da031e78fc55b79515bf982844c49e68b16276
1stprinciple <ztyharbin@gmail.com>
2025-08-08 22:58:06 +0800

    port 81

--- berkeley-function-call-leaderboard/bfcl_eval/model_handler/api_inference/fireworks.py
+++ berkeley-function-call-leaderboard/bfcl_eval/model_handler/api_inference/fireworks.py
@@ -21,7 +21,7 @@ class FireworksHandler(OpenAICompletionsHandler):
         )
         if model_name in ["rlor_model", "rlor/model", "rlor-model"]:
             self.client = OpenAI(
-                base_url="http://localhost:80/v1",
+                base_url="http://localhost:81/v1",
                 api_key=os.getenv("FIREWORKS_API_KEY"),
             )
 
